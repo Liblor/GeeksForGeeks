d37f85c651327fc44132d11a505cb4f9b2a91b16
Loris Reiff <loris.reiff@hotmail.com>
2017-03-07 17:37:52 +0100

    [Add] Sort a stack - 700469

---
+++ 700469_Sort_a_stack/python/sort_a_stack.py
@@ -0,0 +1,10 @@
+# Author: Loris Reiff
+#
+# Challenge: Sort a stack
+# http://www.practice.geeksforgeeks.org/problem-page.php?pid=700469
+#
+# Comment: This challenge does not really make sense in python, way to easy :P
+
+
+def sorted(s):
+    s.sort(reverse=True)
